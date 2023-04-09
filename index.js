ceab4da3ba295e8b592c94c433e1d7d9ec15dbcd
michaelcchu <michael02chu@gmail.com>
2023-04-09 07:40:15 -0400

    attempt to prevent scrolling of score

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ function key(e) {
             && !e.repeat && (document.activeElement.nodeName !== 'INPUT') 
             && (press !== activePress) 
             && splash.classList.contains("splash-toggle")) {
+                e.preventDefault();
                 SheetMusicDisplay.goToNextNote();
                 const note = SheetMusicDisplay.getCurrentNote();            
                 if (note) {
@@ -38,3 +39,16 @@ const containerEventTypes = ["touchstart","touchend"];
 for (const et of containerEventTypes) {main.addEventListener(et, key);}
 const docEventTypes = ["keydown","keyup"];
 for (const et of docEventTypes) {document.addEventListener(et, key);}
+
+/*
+// Turn off default event listeners
+const ets = ['focus', 'pointerover', 'pointerenter', 'pointerdown', 
+  'touchstart', 'gotpointercapture', 'pointermove', 'touchmove', 'pointerup', 
+  'lostpointercapture', 'pointerout', 'pointerleave', 'touchend'];
+for (let et of ets) {
+  main.addEventListener(et, function(event) {
+    event.preventDefault();
+    event.stopPropagation();
+  }, {passive: false}); 
+}
+*/
